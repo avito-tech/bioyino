0c86ef41d48c74c05f7008e2b46943b4c875f0ba
Safronov Michail <msaf1980@gmail.com>
2019-02-14 11:30:37 +0500

    remove delete user after package upgrade/remove

--- contrib/fpm/post_install.sh
+++ contrib/fpm/post_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/usr/bin/getent group bioyino || /usr/sbin/groupadd -r bioyino
-/usr/bin/getent passwd bioyino || /usr/sbin/useradd -d /tmp -M -s /bin/false --system -g bioyino bioyino
+/usr/bin/getent group bioyino || /usr/sbin/groupadd -r bioyino >/dev/null
+/usr/bin/getent passwd bioyino || /usr/sbin/useradd -d /tmp -M -s /bin/false --system -g bioyino bioyino >/dev/null
 [[ -e /bin/systemctl ]] && /bin/systemctl daemon-reload ||:
